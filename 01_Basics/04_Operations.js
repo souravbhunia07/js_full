28b2750e895efdace91340bb2ea9e776df0336c7
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2024-05-21 16:54:03 +0530

    Comparisons

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); // true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); // true
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// Comparisons convert null to a number, treating it as 0
+// console.log(null > 0);  // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+console.log("2" == 2); // true
+console.log("2" === 2); // false (strict check)
+
