ccbaa0577f952d6c166d8a8317856edb40acf580
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2024-05-21 16:43:07 +0530

    Operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -29,5 +29,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ******************* Operations ********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " sourav";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; // assign 4 to all variable
+
+let gameCounter = 100;
+gameCounter++; // Postfix
+++gameCounter; // Prefix
+console.log(gameCounter); // 101
